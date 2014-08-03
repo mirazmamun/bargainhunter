db309d3194b3f1ef4803264c93b19527f84990d3
mirazmamun <mirazmamun@yahoo.com>
2014-08-03 18:45:31 +1000

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', '<the user>');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Alexander#1');
+define('DB_PASSWORD', '<the password>');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
